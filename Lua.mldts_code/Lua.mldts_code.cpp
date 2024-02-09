f735e8f51907cc0f724b1430389065243f5d224b
Макс Титов <hhit934@gmail.com>
2024-02-09 15:06:25 +0300

    asd

--- Lua.mldts_code/Lua.mldts_code.cpp
+++ Lua.mldts_code/Lua.mldts_code.cpp
@@ -1,16 +1,30 @@
 ﻿// Lua.mldts_code.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+
+
 #include <iostream>
 #include<Windows.h>
-#pragma comment(lib , "lua5.1.lib")
-#pragma comment(lib , "lua51.lib")
 
-#include"Lua/include/lua.hpp"
 
  
+
+extern "C" {
+#include "Lua/include/lua.h"
+#include "Lua/include/lualib.h"
+#include "Lua/include/lauxlib.h"
+}
+
+#pragma comment(lib , "lua5.1.lib")
+#pragma comment(lib , "lua51.lib")
 int main()
 {
+    lua_State* lua = luaL_newstate();
+    luaL_openlibs(lua);
+
+    luaL_dofile(lua, "LuaCode/main.lua");
+
+
     std::cout << "Hello World!\n";
 }
 
