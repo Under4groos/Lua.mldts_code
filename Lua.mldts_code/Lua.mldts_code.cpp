bc4e15241f4cc91e01358eb0ed76424542fb0b41
Макс Титов <hhit934@gmail.com>
2024-02-09 14:54:56 +0300

    Добавьте файлы проекта.

---
+++ Lua.mldts_code/Lua.mldts_code.cpp
@@ -0,0 +1,17 @@
+﻿// Lua.mldts_code.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<Windows.h>
+#pragma comment(lib , "lua5.1.lib")
+#pragma comment(lib , "lua51.lib")
+
+#include"Lua/include/lua.hpp"
+
+ 
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+ 
